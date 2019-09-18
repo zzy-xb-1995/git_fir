5ce415dfcc771bb0130974416966e7591071f7d8
zzy <877129011@qq.com>
2019-09-18 17:25:51 +0800

    1.0

---
+++ src/main/java/com/baizhi/git_fir/GitFirApplication.java
@@ -0,0 +1,13 @@
+package com.baizhi.git_fir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitFirApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitFirApplication.class, args);
+    }
+
+}
